b5df81178ea2273056440991464b4fa177a84d28
MohammadAli Khaksar <mohammadali.khaksar.79@gmail.com>
2022-06-30 10:36:50 +0430

    remove unnecessary code from app component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,11 @@ import HomePosts from "./components/home/Posts";
 import React from "react";
 import LayoutsHeader from "./components/layouts/Header";
 import HomePostsPost from "./components/home/Posts/Post";
-import UiFormInput from "./components/ui/Form/Input";
-
-//
-import * as Yup from "yup";
-import { Form, Formik } from "formik";
 
 const App = () => {
   const RouterContent = (component: React.ReactNode) => {
     return component;
   };
-  const SignupSchema = Yup.object().shape({
-    firstName: Yup.string()
-      .min(2, "نام نمیتواند کمتر از 2 کاراکتر باشد")
-      .max(32, "نام نمیتواند بیشتر از 32 کاراکتر باشد")
-      .required("این فیلد ضروری است"),
-  });
 
   return (
     <div>
@@ -35,32 +24,6 @@ const App = () => {
           />
         </Routes>
       </div>
-      {/* <div className="lg:container px-4 lg:my-4 my-2">
-        <h1>Signup</h1>
-
-        <Formik
-          initialValues={{
-            firstName: "",
-          }}
-          validationSchema={SignupSchema}
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form>
-              <UiFormInput
-                errors={{ filed: errors.firstName }}
-                touched={{ filed: touched.firstName }}
-                name="firstName"
-                placeholder="نام"
-              />
-
-              <button type="submit">submit</button>
-            </Form>
-          )}
-        </Formik>
-      </div> */}
     </div>
   );
 };
